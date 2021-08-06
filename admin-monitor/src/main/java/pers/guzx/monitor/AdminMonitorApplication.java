ba76089c2ac89e0cd3d636c35636fc9022785064
guzx <hnguzx@qq.com>
2021-08-06 18:49:23 +0800

    admin监控添加Security后无法修改日志级别

--- admin-monitor/src/main/java/pers/guzx/monitor/AdminMonitorApplication.java
+++ admin-monitor/src/main/java/pers/guzx/monitor/AdminMonitorApplication.java
@@ -3,6 +3,9 @@ package pers.guzx.monitor;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * @author Guzx
  * @version 1.0
@@ -11,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @EnableAdminServer
+@EnableDiscoveryClient
 public class AdminMonitorApplication {
 
     public static void main(String[] args) {
